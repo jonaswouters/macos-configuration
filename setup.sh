1191ac8d7b077ee59aabde4a7f621d9f0f44c6c3
Jonas Wouters <jonas@jw.be>
2022-02-16 13:40:26 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -11,3 +11,20 @@ source ./setup/xcodecli.sh
 # Homebrew
 # ==============
 source ./setup/brew.sh
+
+# ==============
+# SSH & git
+# ==============
+source ./setup/ssh-manager.sh
+
+# TODO
+# - ssh config
+# - asdf config http://asdf-vm.com/guide/getting-started.html#_3-install-asdf
+```
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf install golang latest
+asdf global golang latest
+```
+# - .zprofile
+# - aliasses like 1password login
+# - Fish "default" shell
