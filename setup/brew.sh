85bda0c13410b7264e164b312b62af7bf1511cc0
Jonas Wouters <jonas@jw.be>
2022-02-20 21:02:29 +0100

    testing

--- setup/brew.sh
+++ setup/brew.sh
@@ -3,6 +3,10 @@
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing Hombrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Path
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Updating Homebrew"
     brew update
